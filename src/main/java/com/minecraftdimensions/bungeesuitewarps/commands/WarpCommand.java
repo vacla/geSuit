6bd9059e3e57799d40bee2059b6ad6645465fefb
Bloodsplat <pure__instinct@hotmail.com>
2013-07-31 10:24:46 +1000

    Fixed the warping other player permissions (thanks ravand)  -bungeesuite.warp.other

--- src/main/java/com/minecraftdimensions/bungeesuitewarps/commands/WarpCommand.java
+++ src/main/java/com/minecraftdimensions/bungeesuitewarps/commands/WarpCommand.java
@@ -20,6 +20,9 @@ public class WarpCommand implements CommandExecutor {
 		"bungeesuite.*" };
 	private static final String[] PERMISSION_NODES_OVERRIDE = { "bungeesuite.warp.list.admin",
 		"bungeesuite.admin", "bungeesuite.*" };
+	private static final String[] WARP_OTHER_NODES = { "bungeesuite.warp.other",
+		"bungeesuite.warp.*", "bungeesuite.mod", "bungeesuite.admin",
+		"bungeesuite.*" };
 
 	public WarpCommand(BungeeSuiteWarps bungeeSuiteTeleports) {
 		plugin = bungeeSuiteTeleports;
@@ -39,9 +42,14 @@ public class WarpCommand implements CommandExecutor {
 			}
 			plugin.utils.warpRequest(sender.getName(),sender.getName(), args[0], PermissionUtil.hasPermission(sender, PRIVATE_PERMISSION_NODES));
 			return true;
-		}else if(args.length>=2){
+		}else if(args.length>=2 ){
+			if(PermissionUtil.hasPermission(sender, WARP_OTHER_NODES)){
 			plugin.utils.warpRequest(sender.getName(),args[1], args[0], PermissionUtil.hasPermission(sender, PRIVATE_PERMISSION_NODES));
 			return true;
+			}else{
+				plugin.utils.getMessage(sender.getName(), "WARP_NO_PERMISSION");
+				return true;
+			}
 		}
 		plugin.utils.getWarpList(sender.getName(), PermissionUtil.hasPermission(sender, PERMISSION_NODES_OVERRIDE));
 		return true;
