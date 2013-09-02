2834339bfc91b25e71d05fcaff04cd50f9a9f1a6
bloodsplat <pure__instinct@hotmail.com>
2013-09-02 18:56:17 +1000

    Pretest commit

--- src/main/java/com/minecraftdimensions/bungeesuitehomes/managers/HomesManager.java
+++ src/main/java/com/minecraftdimensions/bungeesuitehomes/managers/HomesManager.java
@@ -1,13 +1,8 @@
 package com.minecraftdimensions.bungeesuitehomes.managers;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Set;
-
+import com.minecraftdimensions.bungeesuitehomes.BungeeSuiteHomes;
+import com.minecraftdimensions.bungeesuitehomes.tasks.PluginMessageTask;
+import com.minecraftdimensions.bungeesuiteteleports.managers.TeleportsManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -16,15 +11,15 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 
-import com.minecraftdimensions.bungeesuitehomes.BungeeSuiteHomes;
-import com.minecraftdimensions.bungeesuitehomes.tasks.PluginMessageTask;
-import com.minecraftdimensions.bungeesuiteteleports.managers.TeleportsManager;
+import java.io.*;
+import java.util.HashMap;
+import java.util.Set;
 
 public class HomesManager {
-	
+
     public static HashMap<String, Location> pendingTeleports = new HashMap<>();
-    
-	public static void deleteHome(CommandSender sender, String home) {
+
+    public static void deleteHome( CommandSender sender, String home ) {
         ByteArrayOutputStream b = new ByteArrayOutputStream();
         DataOutputStream out = new DataOutputStream( b );
         try {
@@ -35,73 +30,68 @@ public class HomesManager {
             e.printStackTrace();
         }
         new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteHomes.instance );
-		
-	}
 
-	public static void sendHome(CommandSender sender, String home) {
+    }
+
+    public static void sendHome( CommandSender sender, String home ) {
         ByteArrayOutputStream b = new ByteArrayOutputStream();
         DataOutputStream out = new DataOutputStream( b );
         try {
             out.writeUTF( "SendPlayerHome" );
             out.writeUTF( sender.getName() );
             out.writeUTF( home );
-            out.writeBoolean(sender.hasPermission("bungeesuite.homes.server"));
-            out.writeBoolean(sender.hasPermission("bungeesuite.homes.global"));
         } catch ( IOException e ) {
             e.printStackTrace();
         }
         new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteHomes.instance );
-		
-	}
 
-	public static void getHomesList(CommandSender sender) {
+    }
+
+    public static void getHomesList( CommandSender sender ) {
         ByteArrayOutputStream b = new ByteArrayOutputStream();
         DataOutputStream out = new DataOutputStream( b );
         try {
             out.writeUTF( "GetHomesList" );
             out.writeUTF( sender.getName() );
-            out.writeBoolean(sender.hasPermission("bungeesuite.homes.server"));
-            out.writeBoolean(sender.hasPermission("bungeesuite.homes.global"));
         } catch ( IOException e ) {
             e.printStackTrace();
         }
         new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteHomes.instance );
-		
-	}
 
-	public static void importHomes(CommandSender sender) {
-		String path = "plugins/Essentials/userdata";
-		File folder = new File(path);
-		File[] listOfFiles = folder.listFiles(new FilenameFilter() {
-			public boolean accept(File dir, String name) {
-				return name.toLowerCase().endsWith(".yml");
-			}
-		});
-		int userCount = 0;
-		int userHomeCount = 0;
-		int homeCount = 0;
-		for (File data : listOfFiles) {
-			userCount++;
-			FileConfiguration user = YamlConfiguration.loadConfiguration(data);
-			if (user.contains("homes")) {
-				userHomeCount++;
-				Set<String> homedata = user.getConfigurationSection("homes")
-						.getKeys(false);
-				if (homedata != null) {
-					for (String homes : homedata) {
-						Location loc = new Location(Bukkit.getWorld(user.getString("homes."+homes+".world")),user.getDouble("homes."+homes+".x"),user.getDouble("homes."+homes+".y"),user.getDouble("homes."+homes+".z"),(float)user.getDouble("homes."+homes+".yaw"),(float)user.getDouble("homes."+homes+".pitch"));
-						setHome(data.getName().substring(0, data.getName().length()-4), homes, loc);
-						homeCount++;
-					}
-				}
-			}
-		}
-		sender.sendMessage(ChatColor.GOLD+"Out of "+userCount+ "users, "+userHomeCount+" had homes");
-		sender.sendMessage(ChatColor.GOLD+"Homes imported: "+homeCount);
-		
-	}
+    }
 
-	public static void reloadHomes(CommandSender sender) {
+    public static void importHomes( CommandSender sender ) {
+        String path = "plugins/Essentials/userdata";
+        File folder = new File( path );
+        File[] listOfFiles = folder.listFiles( new FilenameFilter() {
+            public boolean accept( File dir, String name ) {
+                return name.toLowerCase().endsWith( ".yml" );
+            }
+        } );
+        int userCount = 0;
+        int userHomeCount = 0;
+        int homeCount = 0;
+        for ( File data : listOfFiles ) {
+            userCount++;
+            FileConfiguration user = YamlConfiguration.loadConfiguration( data );
+            if ( user.contains( "homes" ) ) {
+                userHomeCount++;
+                Set<String> homedata = user.getConfigurationSection( "homes" ).getKeys( false );
+                if ( homedata != null ) {
+                    for ( String homes : homedata ) {
+                        Location loc = new Location( Bukkit.getWorld( user.getString( "homes." + homes + ".world" ) ), user.getDouble( "homes." + homes + ".x" ), user.getDouble( "homes." + homes + ".y" ), user.getDouble( "homes." + homes + ".z" ), ( float ) user.getDouble( "homes." + homes + ".yaw" ), ( float ) user.getDouble( "homes." + homes + ".pitch" ) );
+                        setHome( data.getName().substring( 0, data.getName().length() - 4 ), homes, loc );
+                        homeCount++;
+                    }
+                }
+            }
+        }
+        sender.sendMessage( ChatColor.GOLD + "Out of " + userCount + "users, " + userHomeCount + " had homes" );
+        sender.sendMessage( ChatColor.GOLD + "Homes imported: " + homeCount );
+
+    }
+
+    public static void reloadHomes( CommandSender sender ) {
         ByteArrayOutputStream b = new ByteArrayOutputStream();
         DataOutputStream out = new DataOutputStream( b );
         try {
@@ -111,113 +101,111 @@ public class HomesManager {
             e.printStackTrace();
         }
         new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteHomes.instance );
-		
-	}
 
-	public static void setHome(CommandSender sender, String home) {
+    }
+
+    public static void setHome( CommandSender sender, String home ) {
         ByteArrayOutputStream b = new ByteArrayOutputStream();
         DataOutputStream out = new DataOutputStream( b );
-        Location l = ((Player)sender).getLocation();
+        Location l = ( ( Player ) sender ).getLocation();
         try {
             out.writeUTF( "SetPlayersHome" );
             out.writeUTF( sender.getName() );
-            out.writeInt(getServerHomesLimit(sender));
-            out.writeInt(getGlobalHomesLimit(sender));
+            out.writeInt( getServerHomesLimit( sender ) );
+            out.writeInt( getGlobalHomesLimit( sender ) );
             out.writeUTF( home );
-            out.writeUTF(l.getWorld().getName());
-            out.writeDouble(l.getX());
-            out.writeDouble(l.getY());
-            out.writeDouble(l.getZ());
-            out.writeFloat(l.getYaw());
-            out.writeFloat(l.getPitch());
+            out.writeUTF( l.getWorld().getName() );
+            out.writeDouble( l.getX() );
+            out.writeDouble( l.getY() );
+            out.writeDouble( l.getZ() );
+            out.writeFloat( l.getYaw() );
+            out.writeFloat( l.getPitch() );
         } catch ( IOException e ) {
             e.printStackTrace();
         }
         new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteHomes.instance );
-		
-	}
-	
-	public static void setHome(String sender, String home, Location l) {
+
+    }
+
+    public static void setHome( String sender, String home, Location l ) {
         ByteArrayOutputStream b = new ByteArrayOutputStream();
         DataOutputStream out = new DataOutputStream( b );
         try {
             out.writeUTF( "SetPlayersHome" );
             out.writeUTF( sender );
             out.writeUTF( home );
-            out.writeInt(100);
-            out.writeInt(300);
-            out.writeUTF(l.getWorld().getName());
-            out.writeDouble(l.getX());
-            out.writeDouble(l.getY());
-            out.writeDouble(l.getZ());
-            out.writeFloat(l.getYaw());
-            out.writeFloat(l.getPitch());
+            out.writeInt( 100 );
+            out.writeInt( 300 );
+            out.writeUTF( l.getWorld().getName() );
+            out.writeDouble( l.getX() );
+            out.writeDouble( l.getY() );
+            out.writeDouble( l.getZ() );
+            out.writeFloat( l.getYaw() );
+            out.writeFloat( l.getPitch() );
         } catch ( IOException e ) {
             e.printStackTrace();
         }
         new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteHomes.instance );
-		
-		
-	}
-	
-	public static int getServerHomesLimit(CommandSender p) {
-		int max = 0;
 
-		int maxlimit = 100;
 
-		if (p.hasPermission("bungeesuite.homes.limits.server*") || p.hasPermission("bungeesuite.homes.limits.*")) {
-			return maxlimit;
-		} else {
-			for (int ctr = 0; ctr < maxlimit; ctr++) {
-				if (p.hasPermission("bungeesuite.homes.limits.server" + ctr)) {
-					max = ctr;
-				}
-			}
+    }
 
-		}
+    public static int getServerHomesLimit( CommandSender p ) {
+        int max = 0;
 
-		return max;
-	}
-	
-	public static int getGlobalHomesLimit(CommandSender p) {
-		int max = 0;
+        int maxlimit = 100;
 
-		int maxlimit = 300;
+        if ( p.hasPermission( "bungeesuite.homes.limits.server*" ) || p.hasPermission( "bungeesuite.homes.limits.*" ) ) {
+            return maxlimit;
+        } else {
+            for ( int ctr = 0; ctr < maxlimit; ctr++ ) {
+                if ( p.hasPermission( "bungeesuite.homes.limits.server" + ctr ) ) {
+                    max = ctr;
+                }
+            }
 
-		if (p.hasPermission("bungeesuite.homes.limits.global.*") || p.hasPermission("bungeesuite.homes.limits.*")) {
-			return maxlimit;
-		} else {
-			for (int ctr = 0; ctr < maxlimit; ctr++) {
-				if (p.hasPermission("bungeesuite.homes.limits.global" + ctr)) {
-					max = ctr;
-				}
-			}
+        }
 
-		}
-		return max;
-	}
+        return max;
+    }
 
-	public static void teleportPlayerToLocation(final String player,
-			Location location) {
-		Player p = Bukkit.getPlayer(player);
-		if (p != null) {
-			p.teleport(location);
-		} else {
-			pendingTeleports.put(player, location);
-			if (BungeeSuiteHomes.usingTeleports) {
-				TeleportsManager.ignoreTeleport.add(p);
-			}
-			Bukkit.getScheduler().runTaskLaterAsynchronously(
-					BungeeSuiteHomes.instance, new Runnable() {
-						@Override
-						public void run() {
-							if (pendingTeleports.containsKey(player)) {
-								pendingTeleports.remove(player);
-							}
-						}
-					}, 100);
-		}
-		
-	}
+    public static int getGlobalHomesLimit( CommandSender p ) {
+        int max = 0;
+
+        int maxlimit = 300;
+
+        if ( p.hasPermission( "bungeesuite.homes.limits.global.*" ) || p.hasPermission( "bungeesuite.homes.limits.*" ) ) {
+            return maxlimit;
+        } else {
+            for ( int ctr = 0; ctr < maxlimit; ctr++ ) {
+                if ( p.hasPermission( "bungeesuite.homes.limits.global" + ctr ) ) {
+                    max = ctr;
+                }
+            }
+
+        }
+        return max;
+    }
+
+    public static void teleportPlayerToLocation( final String player, Location location ) {
+        Player p = Bukkit.getPlayer( player );
+        if ( p != null ) {
+            p.teleport( location );
+        } else {
+            pendingTeleports.put( player, location );
+            if ( BungeeSuiteHomes.usingTeleports ) {
+                TeleportsManager.ignoreTeleport.add( p );
+            }
+            Bukkit.getScheduler().runTaskLaterAsynchronously( BungeeSuiteHomes.instance, new Runnable() {
+                @Override
+                public void run() {
+                    if ( pendingTeleports.containsKey( player ) ) {
+                        pendingTeleports.remove( player );
+                    }
+                }
+            }, 100 );
+        }
+
+    }
 
 }
