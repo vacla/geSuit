55166e08acb3d23350ebd15701a4f6bf4b8fc9e2
bloodsplat <pure__instinct@hotmail.com>
2013-10-13 18:43:45 +1100

    Update the /bsversion command to send to the BungeeCord

--- src/main/java/com/minecraftdimensions/bungeesuitebans/BansMessageListener.java
+++ src/main/java/com/minecraftdimensions/bungeesuitebans/BansMessageListener.java
@@ -1,5 +1,6 @@
 package com.minecraftdimensions.bungeesuitebans;
 
+import com.minecraftdimensions.bungeesuitebans.managers.BansManager;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
@@ -18,18 +19,28 @@ public class BansMessageListener implements PluginMessageListener {
     @Override
     public void onPluginMessageReceived( String channel, Player player, byte[] message ) {
         DataInputStream in = new DataInputStream( new ByteArrayInputStream( message ) );
-        String task;
+        String task = null;
 
         try {
             task = in.readUTF();
-            if ( task.equals( "GetVersion" ) ) {
-                Player p = Bukkit.getPlayer( in.readUTF() );
-                p.sendMessage( ChatColor.RED + "Bans - " + ChatColor.GOLD + BungeeSuiteBans.instance.getDescription().getVersion() );
-                Bukkit.getConsoleSender().sendMessage( ChatColor.RED + "Bans - " + ChatColor.GOLD + BungeeSuiteBans.instance.getDescription().getVersion() );
-            }
 
         } catch ( IOException e ) {
             e.printStackTrace();
+        }
+
+        if ( task.equals( "GetVersion" ) ) {
+            String name = null;
+            try {
+                name = in.readUTF();
+            } catch ( IOException e ) {
+
+            }
+            if ( name != null ) {
+                Player p = Bukkit.getPlayer( name );
+                p.sendMessage( ChatColor.RED + "Bans - " + ChatColor.GOLD + BungeeSuiteBans.instance.getDescription().getVersion() );
+            }
+            Bukkit.getConsoleSender().sendMessage( ChatColor.RED + "Bans - " + ChatColor.GOLD + BungeeSuiteBans.instance.getDescription().getVersion() );
+            BansManager.sendVersion();
         }
     }
 }
--- src/main/java/com/minecraftdimensions/bungeesuitebans/managers/BansManager.java
+++ src/main/java/com/minecraftdimensions/bungeesuitebans/managers/BansManager.java
@@ -1,180 +1,181 @@
 package com.minecraftdimensions.bungeesuitebans.managers;
 
+import com.minecraftdimensions.bungeesuitebans.BungeeSuiteBans;
+import com.minecraftdimensions.bungeesuitebans.tasks.PluginMessageTask;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+
 import java.io.ByteArrayOutputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-
-import com.minecraftdimensions.bungeesuitebans.BungeeSuiteBans;
-import com.minecraftdimensions.bungeesuitebans.tasks.PluginMessageTask;
-
-
-
 
 public class BansManager {
-	
-	public static void banPlayer(String sender, String player, String msg) {
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		DataOutputStream out = new DataOutputStream(b);
-		try {
-			out.writeUTF("BanPlayer");
-			out.writeUTF(sender);
-			out.writeUTF(player);
-			out.writeUTF(msg);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		new PluginMessageTask(b).runTaskAsynchronously(BungeeSuiteBans.instance);	
-	}
 
-	public static void kickAll(String sender,String msg) {
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		DataOutputStream out = new DataOutputStream(b);
-		try {
-			out.writeUTF("KickAll");
-			out.writeUTF(sender);
-			out.writeUTF(msg);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		new PluginMessageTask(b).runTaskAsynchronously(BungeeSuiteBans.instance);
-		
-	}
+    public static void banPlayer( String sender, String player, String msg ) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "BanPlayer" );
+            out.writeUTF( sender );
+            out.writeUTF( player );
+            out.writeUTF( msg );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
+    }
 
-	public static void kickPlayer(String sender, String player, String msg) {
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		DataOutputStream out = new DataOutputStream(b);
-		try {
-			out.writeUTF("KickPlayer");
-			out.writeUTF(sender);
-			out.writeUTF(player);
-			out.writeUTF(msg);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		new PluginMessageTask(b).runTaskAsynchronously(BungeeSuiteBans.instance);
-		
-	}
+    public static void kickAll( String sender, String msg ) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "KickAll" );
+            out.writeUTF( sender );
+            out.writeUTF( msg );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
 
-	public static void tempBanPlayer(String sender, String player, String timing, Command command) {
-		String [] arg1 = timing.split(" "); 
-		int minuteIncrease = 0;
-		int hourIncrease = 0;
-		int dateIncrease = 0;
-		String message = "";
-		for (int i = 1; i < arg1.length; i++) {
-			try {
-				if(arg1[i].length()>1){
-				if (arg1[i].substring(0, 2).equalsIgnoreCase("d:")) {
-					dateIncrease += Integer.parseInt(arg1[i].substring(
-							2, arg1[i].length()));
-				}else
-				if (arg1[i].substring(0, 2).equalsIgnoreCase("h:")) {
-					hourIncrease += Integer.parseInt(arg1[i].substring(
-							2, arg1[i].length()));
-				}else
-				if (arg1[i].substring(0, 2).equalsIgnoreCase("m:")) {
-					minuteIncrease += Integer.parseInt(arg1[i]
-							.substring(2, arg1[i].length()));
-				}else{
-					message+=arg1[i]+" ";
-				}
-				}else{
-					message+=arg1[i]+" ";
-				}
-			} catch (NumberFormatException e) {
-				Bukkit.getPlayer(sender).sendMessage(command.getUsage());
-				return;
-			}
-		}
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		DataOutputStream out = new DataOutputStream(b);
-		try {
-			out.writeUTF("TempBanPlayer");
-			out.writeUTF(sender);
-			out.writeUTF(player);
-			out.writeInt(minuteIncrease);
-			out.writeInt(hourIncrease);
-			out.writeInt(dateIncrease);
-			out.writeUTF(message);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		new PluginMessageTask(b).runTaskAsynchronously(BungeeSuiteBans.instance);
-	}
+    }
 
-	public static void unbanPlayer(String sender, String player) {
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		DataOutputStream out = new DataOutputStream(b);
-		try {
-			out.writeUTF("UnbanPlayer");
-			out.writeUTF(sender);
-			out.writeUTF(player);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		new PluginMessageTask(b).runTaskAsynchronously(BungeeSuiteBans.instance);
-		
-	}
+    public static void kickPlayer( String sender, String player, String msg ) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "KickPlayer" );
+            out.writeUTF( sender );
+            out.writeUTF( player );
+            out.writeUTF( msg );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
 
-	public static void reloadBans(String string) {
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		DataOutputStream out = new DataOutputStream(b);
-		try {
-			out.writeUTF("ReloadBans");
-			out.writeUTF(string);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		new PluginMessageTask(b)
-				.runTaskAsynchronously(BungeeSuiteBans.instance);	
-	}
-	
-	public static void ipBanPlayer(String sender, String player, String msg) {
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		DataOutputStream out = new DataOutputStream(b);
-		try {
-			out.writeUTF("IPBanPlayer");
-			out.writeUTF(sender);
-			out.writeUTF(player);
-			out.writeUTF(msg);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		new PluginMessageTask(b).runTaskAsynchronously(BungeeSuiteBans.instance);
-		
-	}
-	
-	public static void unipBanPlayer(String sender, String player, String msg) {
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		DataOutputStream out = new DataOutputStream(b);
-		try {
-			out.writeUTF("UnIPBanPlayer");
-			out.writeUTF(sender);
-			out.writeUTF(player);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		new PluginMessageTask(b).runTaskAsynchronously(BungeeSuiteBans.instance);
-		
-	}
-	
-	public static void checkPlayerBans(String sender, String player) {
-		ByteArrayOutputStream b = new ByteArrayOutputStream();
-		DataOutputStream out = new DataOutputStream(b);
-		try {
-			out.writeUTF("CheckPlayerBans");
-			out.writeUTF(sender);
-			out.writeUTF(player);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		new PluginMessageTask(b).runTaskAsynchronously(BungeeSuiteBans.instance);
-	}
+    }
 
-	
-	
+    public static void tempBanPlayer( String sender, String player, String timing, Command command ) {
+        String[] arg1 = timing.split( " " );
+        int minuteIncrease = 0;
+        int hourIncrease = 0;
+        int dateIncrease = 0;
+        String message = "";
+        for ( int i = 1; i < arg1.length; i++ ) {
+            try {
+                if ( arg1[i].length() > 1 ) {
+                    if ( arg1[i].substring( 0, 2 ).equalsIgnoreCase( "d:" ) ) {
+                        dateIncrease += Integer.parseInt( arg1[i].substring( 2, arg1[i].length() ) );
+                    } else if ( arg1[i].substring( 0, 2 ).equalsIgnoreCase( "h:" ) ) {
+                        hourIncrease += Integer.parseInt( arg1[i].substring( 2, arg1[i].length() ) );
+                    } else if ( arg1[i].substring( 0, 2 ).equalsIgnoreCase( "m:" ) ) {
+                        minuteIncrease += Integer.parseInt( arg1[i].substring( 2, arg1[i].length() ) );
+                    } else {
+                        message += arg1[i] + " ";
+                    }
+                } else {
+                    message += arg1[i] + " ";
+                }
+            } catch ( NumberFormatException e ) {
+                Bukkit.getPlayer( sender ).sendMessage( command.getUsage() );
+                return;
+            }
+        }
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "TempBanPlayer" );
+            out.writeUTF( sender );
+            out.writeUTF( player );
+            out.writeInt( minuteIncrease );
+            out.writeInt( hourIncrease );
+            out.writeInt( dateIncrease );
+            out.writeUTF( message );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
+    }
 
+    public static void unbanPlayer( String sender, String player ) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "UnbanPlayer" );
+            out.writeUTF( sender );
+            out.writeUTF( player );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
+
+    }
+
+    public static void reloadBans( String string ) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "ReloadBans" );
+            out.writeUTF( string );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
+    }
+
+    public static void ipBanPlayer( String sender, String player, String msg ) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "IPBanPlayer" );
+            out.writeUTF( sender );
+            out.writeUTF( player );
+            out.writeUTF( msg );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
+
+    }
+
+    public static void unipBanPlayer( String sender, String player, String msg ) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "UnIPBanPlayer" );
+            out.writeUTF( sender );
+            out.writeUTF( player );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
+
+    }
+
+    public static void checkPlayerBans( String sender, String player ) {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "CheckPlayerBans" );
+            out.writeUTF( sender );
+            out.writeUTF( player );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
+    }
+
+
+    public static void sendVersion() {
+        ByteArrayOutputStream b = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream( b );
+        try {
+            out.writeUTF( "SendVersion" );
+            out.writeUTF( ChatColor.RED + "Bans - " + ChatColor.GOLD + BungeeSuiteBans.instance.getDescription().getVersion() );
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+        new PluginMessageTask( b ).runTaskAsynchronously( BungeeSuiteBans.instance );
+    }
 }
