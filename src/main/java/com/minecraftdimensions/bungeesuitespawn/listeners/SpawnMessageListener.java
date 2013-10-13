30c1ce81525ed082cbc883ea470e46fd9258c314
bloodsplat <pure__instinct@hotmail.com>
2013-10-13 17:08:25 +1100

    Add /bsversion support to all modules

--- src/main/java/com/minecraftdimensions/bungeesuitespawn/listeners/SpawnMessageListener.java
+++ src/main/java/com/minecraftdimensions/bungeesuitespawn/listeners/SpawnMessageListener.java
@@ -1,38 +1,45 @@
 package com.minecraftdimensions.bungeesuitespawn.listeners;
 
+import com.minecraftdimensions.bungeesuitespawn.BungeeSuiteSpawn;
+import com.minecraftdimensions.bungeesuitespawn.managers.SpawnManager;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.messaging.PluginMessageListener;
+
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 
-
-import org.bukkit.entity.Player;
-import org.bukkit.event.Listener;
-import org.bukkit.plugin.messaging.PluginMessageListener;
-import com.minecraftdimensions.bungeesuitespawn.managers.SpawnManager;
-
 public class SpawnMessageListener implements PluginMessageListener, Listener {
 
-	@Override
-	public void onPluginMessageReceived(String channel, Player player,
-			byte[] message) {
-		DataInputStream in = new DataInputStream(new ByteArrayInputStream(
-				message));
-		String task = null;
+    @Override
+    public void onPluginMessageReceived( String channel, Player player, byte[] message ) {
+        DataInputStream in = new DataInputStream( new ByteArrayInputStream( message ) );
+        String task;
 
-		try {
-			task = in.readUTF();
-			if(task.equals("SendSpawn")){
-				SpawnManager.addSpawn(in.readUTF(),in.readUTF(),in.readDouble(),in.readDouble(),in.readDouble(),in.readFloat(),in.readFloat());
-			}else if ( task.equals("TeleportToLocation")){
-				SpawnManager.teleportPlayer(in.readUTF(), in.readUTF(), in.readDouble(), in.readDouble(), in.readDouble(), in.readFloat(), in.readFloat());
-			}
-				
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		
-	}
+        try {
+            task = in.readUTF();
+            switch ( task ) {
+                case "SendSpawn":
+                    SpawnManager.addSpawn( in.readUTF(), in.readUTF(), in.readDouble(), in.readDouble(), in.readDouble(), in.readFloat(), in.readFloat() );
+                    break;
+                case "TeleportToLocation":
+                    SpawnManager.teleportPlayer( in.readUTF(), in.readUTF(), in.readDouble(), in.readDouble(), in.readDouble(), in.readFloat(), in.readFloat() );
+                    break;
+                case "GetVersion":
+                    Player p = Bukkit.getPlayer( in.readUTF() );
+                    p.sendMessage( ChatColor.RED + "Spawns - " + ChatColor.GOLD + BungeeSuiteSpawn.INSTANCE.getDescription().getVersion() );
+                    Bukkit.getConsoleSender().sendMessage( ChatColor.RED + "Spawns - " + ChatColor.GOLD + BungeeSuiteSpawn.INSTANCE.getDescription().getVersion() );
+                    break;
+            }
 
-	
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
+
+    }
+
 
 }
