2d6366dc1f22cc8190d82ba69411d571b499af91
bloodsplat <pure__instinct@hotmail.com>
2013-10-13 17:08:26 +1100

    Add /bsversion support to all modules

--- src/main/java/com/minecraftdimensions/bungeesuitewarps/listeners/WarpsMessageListener.java
+++ src/main/java/com/minecraftdimensions/bungeesuitewarps/listeners/WarpsMessageListener.java
@@ -1,40 +1,38 @@
 package com.minecraftdimensions.bungeesuitewarps.listeners;
 
-import java.io.ByteArrayInputStream;
-import java.io.DataInputStream;
-import java.io.IOException;
-
+import com.minecraftdimensions.bungeesuiteteleports.BungeeSuiteTeleports;
+import com.minecraftdimensions.bungeesuitewarps.managers.WarpsManager;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.messaging.PluginMessageListener;
 
-import com.minecraftdimensions.bungeesuitewarps.managers.WarpsManager;
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.IOException;
 
 public class WarpsMessageListener implements PluginMessageListener {
 
-	@Override
-	public void onPluginMessageReceived(String channel, Player player,
-			byte[] message) {
-		DataInputStream in = new DataInputStream(new ByteArrayInputStream(
-				message));
-		String task = null;
+    @Override
+    public void onPluginMessageReceived( String channel, Player player, byte[] message ) {
+        DataInputStream in = new DataInputStream( new ByteArrayInputStream( message ) );
+        String task;
 
-		try {
-			task = in.readUTF();
+        try {
+            task = in.readUTF();
 
-			if (task.equals("TeleportPlayerToLocation")) {
-				WarpsManager
-						.teleportPlayerToWarp(
-								in.readUTF(),
-								new Location(Bukkit.getWorld(in.readUTF()), in
-										.readDouble(), in.readDouble(), in
-										.readDouble(), in.readFloat(), in
-										.readFloat()));
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+            if ( task.equals( "TeleportPlayerToLocation" ) ) {
+                WarpsManager.teleportPlayerToWarp( in.readUTF(), new Location( Bukkit.getWorld( in.readUTF() ), in.readDouble(), in.readDouble(), in.readDouble(), in.readFloat(), in.readFloat() ) );
+            }
+            if ( task.equals( "GetVersion" ) ) {
+                Player p = Bukkit.getPlayer( in.readUTF() );
+                p.sendMessage( ChatColor.RED + "Warps - " + ChatColor.GOLD + BungeeSuiteTeleports.instance.getDescription().getVersion() );
+                Bukkit.getConsoleSender().sendMessage( ChatColor.RED + "Warps - " + ChatColor.GOLD + BungeeSuiteTeleports.instance.getDescription().getVersion() );
+            }
+        } catch ( IOException e ) {
+            e.printStackTrace();
+        }
 
-	}
+    }
 }
