3b4511b1bc5385e49bb66c619fec6aff1b44c87e
Hatventures <webmaster@hatventures.net>
2014-03-17 19:08:37 +0100

    - Cleanup custom event - formating

--- src/main/java/net/cubespace/geSuit/pluginmessages/TeleportToLocation.java
+++ src/main/java/net/cubespace/geSuit/pluginmessages/TeleportToLocation.java
@@ -1,21 +1,23 @@
 package net.cubespace.geSuit.pluginmessages;
 
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
 import net.cubespace.geSuit.geSuit;
 import net.cubespace.geSuit.objects.GSPlayer;
 import net.cubespace.geSuit.objects.Location;
 import net.cubespace.geSuit.tasks.SendPluginMessage;
 
-import java.io.ByteArrayOutputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
-
 /**
  * @author geNAZt (fabian.fassbender42@googlemail.com)
  */
-public class TeleportToLocation {
+public class TeleportToLocation
+{
+
     public static String OUTGOING_CHANNEL = "geSuitTeleport";
 
-    public static void execute(GSPlayer player, Location location) {
+    public static void execute(GSPlayer player, Location location)
+    {
         if (player.getServer() == null || !player.getServer().equals(location.getServer().getName())) {
             player.getProxiedPlayer().connect(location.getServer());
         }
@@ -32,7 +34,8 @@ public class TeleportToLocation {
             out.writeDouble(location.getZ());
             out.writeFloat(location.getYaw());
             out.writeFloat(location.getPitch());
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             e.printStackTrace();
         }
 
