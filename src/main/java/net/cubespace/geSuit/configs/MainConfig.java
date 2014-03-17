894713f97c3515e42c4e94d3b56ee9fa048388fb
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2014-03-17 21:13:03 +0100

    Fix the MySQL Error in the Update Script

--- src/main/java/net/cubespace/geSuit/configs/MainConfig.java
+++ src/main/java/net/cubespace/geSuit/configs/MainConfig.java
@@ -31,7 +31,7 @@ public class MainConfig extends Config {
     public Boolean Inited = false;
 
     @Comment("Stored version informations. If you alter this you can damage your Database")
-    public Integer Version_Database_Ban = 2;
+    public Integer Version_Database_Ban = 3;
     @Comment("Stored version informations. If you alter this you can damage your Database")
     public Integer Version_Database_Homes = 2;
     @Comment("Stored version informations. If you alter this you can damage your Database")
