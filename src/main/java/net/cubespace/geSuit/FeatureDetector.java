1351eda12ba8ac0803f8133d2947bafaaf5961d3
Fabian Fassbender <fabian.fassbender42@googlemail.com>
2014-03-28 20:47:40 +0100

    Maybe fixed a Bug in getUUID

--- src/main/java/net/cubespace/geSuit/FeatureDetector.java
+++ src/main/java/net/cubespace/geSuit/FeatureDetector.java
@@ -11,7 +11,7 @@ public class FeatureDetector {
 
     static {
         try {
-            ProxiedPlayer.class.getMethod("getUUID");
+            ProxiedPlayer.class.getMethod("getUUID", String.class);
 
             useUUID = !ConfigManager.main.OverwriteUUID;
         } catch (NoSuchMethodException e) {
