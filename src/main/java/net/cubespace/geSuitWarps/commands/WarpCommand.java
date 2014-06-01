aebccafddf1451e696e4144a4cc96329e7fa63f1
mineyourmind-admin <CXLBDkVdgGxGnhf9B2o0>
2014-06-01 14:52:31 +0200

    save data on command use

--- src/main/java/net/cubespace/geSuitWarps/commands/WarpCommand.java
+++ src/main/java/net/cubespace/geSuitWarps/commands/WarpCommand.java
@@ -1,5 +1,6 @@
 package net.cubespace.geSuitWarps.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,8 +14,9 @@ public class WarpCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
+        Bukkit.getPlayer(sender.getName()).saveData();
 		if(args.length==1){
-			WarpsManager.warpPlayer(sender, sender.getName(), args[0]);
+            WarpsManager.warpPlayer(sender, sender.getName(), args[0]);
 			return true;
 		}else if(args.length>1 && sender.hasPermission("gesuit.warps.command.warp.other")){
 			WarpsManager.warpPlayer(sender, args[0], args[1]);
