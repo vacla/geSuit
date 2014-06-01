5c361ea6b43a86db21dcd9606e5610ebe2ab683b
mineyourmind-admin <CXLBDkVdgGxGnhf9B2o0>
2014-06-01 14:22:05 +0200

    added saveData on home command use

--- src/main/java/net/cubespace/geSuitHomes/commands/HomeCommand.java
+++ src/main/java/net/cubespace/geSuitHomes/commands/HomeCommand.java
@@ -1,5 +1,6 @@
 package net.cubespace.geSuitHomes.commands;
 
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -11,9 +12,12 @@ public class HomeCommand implements CommandExecutor {
 	@Override
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
+
+        Bukkit.getPlayer(sender.getName()).saveData();
 		if(args.length==0){
-			HomesManager.sendHome(sender, "home");
+            HomesManager.sendHome(sender, "home");
 		}else{
+
 			HomesManager.sendHome(sender, args[0]);
 		}
 		return true;
