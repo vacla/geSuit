486fdb7d9e35d26394d436cf53a7743730219982
Antony Riley <antony@cyberiantiger.org>
2014-06-17 11:26:03 +0300

    Fix NPE in burn prevention around portals.

--- src/main/java/net/cubespace/geSuitPortals/listeners/AntiBurnListener.java
+++ src/main/java/net/cubespace/geSuitPortals/listeners/AntiBurnListener.java
@@ -1,5 +1,7 @@
 package net.cubespace.geSuitPortals.listeners;
 
+import java.util.ArrayList;
+import java.util.List;
 import net.cubespace.geSuitPortals.managers.PortalsManager;
 import net.cubespace.geSuitPortals.objects.Portal;
 import org.bukkit.Location;
@@ -41,16 +43,16 @@ public class AntiBurnListener implements Listener {
 
     private boolean checkIfInPortal(Block block) {
         if (canIgnite(block)) {
-            for (Portal p : PortalsManager.PORTALS.get(block.getWorld())) {
-                if (p.isBlockInPortal(block)) {
-                    return true;
+            List<Portal> portals = PortalsManager.PORTALS.get(block.getWorld());
+            if (portals != null) {
+                for (Portal p : PortalsManager.PORTALS.get(block.getWorld())) {
+                    if (p.isBlockInPortal(block)) {
+                        return true;
+                    }
                 }
             }
-
-            return false;
-        } else {
-            return false;
         }
+        return false;
     }
 
     private boolean canIgnite(Block block) {
