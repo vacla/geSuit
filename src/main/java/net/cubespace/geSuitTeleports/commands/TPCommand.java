ad0ecef560d85829ed69b1f769154c4fbd1ac9c4
mineyourmind-admin <CXLBDkVdgGxGnhf9B2o0>
2014-06-18 22:58:41 +0200

    derp..

--- src/main/java/net/cubespace/geSuitTeleports/commands/TPCommand.java
+++ src/main/java/net/cubespace/geSuitTeleports/commands/TPCommand.java
@@ -16,46 +16,46 @@ public class TPCommand implements CommandExecutor {
 
     @Override
     public boolean onCommand( CommandSender sender, Command command, String label, String[] args ) {
-        try {
-            Bukkit.getPlayer(sender.getName()).saveData();
-            if (args[0] != null) {
-                Bukkit.getPlayer(args[0]).saveData();
-            }
-        } catch (Exception e) {
-            geSuitTeleports.getInstance().getLogger().log(Level.SEVERE, "Could not save player data!", e);
-        }
 
         if ( !( sender instanceof Player ) ) {
             if ( args.length == 2 ) {
+                Bukkit.getPlayer(args[0]).saveData();
                 Bukkit.getPlayer( args[0] ).teleport( Bukkit.getPlayer( args[1] ) );
             } else if ( args.length == 4 ) {
                 Player p = Bukkit.getPlayer( args[0] );
+                p.saveData();
                 p.teleport( new Location( p.getWorld(), Double.parseDouble( args[1] ), Double.parseDouble( args[2] ), Double.parseDouble( args[3] ) ) );
             }
         }
         if ( args.length == 1 ) {
+            Bukkit.getPlayer(sender.getName()).saveData();
             TeleportsManager.teleportToPlayer( sender, sender.getName(), args[0] );
             return true;
 
         } else if ( args.length == 2 ) {
+            Bukkit.getPlayer(args[0]).saveData();
             TeleportsManager.teleportToPlayer( sender, args[0], args[1] );
             return true;
         }
 
         if ( args.length == 3 ) {
+            Bukkit.getPlayer(sender.getName()).saveData();
             TeleportsManager.teleportToLocation(sender.getName(), "", ((Player) sender).getWorld().getName(), Double.valueOf(args[0]), Double.valueOf(args[1]), Double.valueOf(args[2]));
             return true;
         }
 
         if ( args.length == 4) {
+            Bukkit.getPlayer(sender.getName()).saveData();
             TeleportsManager.teleportToLocation(sender.getName(), "", args[3], Double.valueOf(args[0]), Double.valueOf(args[1]), Double.valueOf(args[2]));
             return true;
         }
 
         if ( args.length == 5 ) {
             if ( Bukkit.getPlayer( args[0] ) != null ) {
+                Bukkit.getPlayer(args[0]).saveData();
                 TeleportsManager.teleportToLocation( args[0], "", args[4], Double.valueOf( args[1] ), Double.valueOf( args[2] ), Double.valueOf( args[3] ) );
             } else {
+                Bukkit.getPlayer(sender.getName()).saveData();
                 TeleportsManager.teleportToLocation( sender.getName(), args[0], args[1], Double.valueOf( args[2] ), Double.valueOf( args[3] ), Double.valueOf( args[4] ) );
             }
 
