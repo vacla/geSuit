49ccf569965e4cb2e7e3d5d996c368b6b755a199
addstar <addstar@addstar.com.au>
2014-08-15 16:41:39 +1000

    Build against bukkit-1.7.10

--- src/main/java/net/cubespace/geSuitHomes/tasks/PluginMessageTask.java
+++ src/main/java/net/cubespace/geSuitHomes/tasks/PluginMessageTask.java
@@ -1,7 +1,10 @@
 package net.cubespace.geSuitHomes.tasks;
 
 import net.cubespace.geSuitHomes.geSuitHomes;
+
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import java.io.ByteArrayOutputStream;
@@ -15,11 +18,15 @@ public class PluginMessageTask extends BukkitRunnable {
 	}
 	
 	public void run() {
-			Bukkit.getOnlinePlayers()[0].sendPluginMessage(
+		Player player = Bukkit.getOnlinePlayers().iterator().next();
+		if (player != null) {
+			player.sendPluginMessage(
 					geSuitHomes.instance,
 					"geSuitHomes",
 					bytes.toByteArray());
-	
+		} else {
+			System.out.println(ChatColor.RED + "Unable to send Plugin Message - No players online.");
+		}
 	}
 
 }
