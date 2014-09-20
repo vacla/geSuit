d4933e5bbfc9461202317d1ec0f66f6f1ca9203e
addstar <addstar@addstar.com.au>
2014-09-21 01:16:09 +1000

    Actually forgot to update /spawn too.. now it has nice messages.

--- src/main/java/net/cubespace/geSuiteSpawn/commands/SpawnCommand.java
+++ src/main/java/net/cubespace/geSuiteSpawn/commands/SpawnCommand.java
@@ -1,8 +1,13 @@
 package net.cubespace.geSuiteSpawn.commands;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import net.cubespace.geSuiteSpawn.geSuitSpawn;
 import net.cubespace.geSuiteSpawn.managers.SpawnManager;
 
 public class SpawnCommand implements CommandExecutor {
@@ -11,9 +16,28 @@ public class SpawnCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command command,
 			String label, String[] args) {
 
-			SpawnManager.sendPlayerToSpawn(sender);
-	
-		return true;
+		final Player player = Bukkit.getPlayer(sender.getName());
+        if (!player.hasPermission("gesuit.warps.bypass.delay")) {
+            final Location loc = player.getLocation();
+
+            player.sendMessage("&6Teleportation will commence in &c3 seconds&6. Don't move.");
+            Bukkit.getServer().getScheduler().runTaskLater(geSuitSpawn.INSTANCE, new Runnable() {
+                @Override
+                public void run() {
+                    if (loc.getBlock().equals(player.getLocation().getBlock())) {
+                        player.sendMessage("&6Teleportation commencing...");
+            			SpawnManager.sendPlayerToSpawn(player);
+                    } else {
+                        player.sendMessage("&cTeleportation aborted because you moved.");
+                    }
+                }
+            }, 60L);
+            return true;
+        } else {
+			SpawnManager.sendPlayerToSpawn(player);
+            return true;
+        }
+
 	}
 
 }
