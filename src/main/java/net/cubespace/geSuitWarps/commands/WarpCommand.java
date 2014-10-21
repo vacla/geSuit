b9d52b40a447d2384da6b608244a7b78f44ad051
addstar <addstar@addstar.com.au>
2014-10-21 21:35:21 +1100

    Fix possible memory leak - remove the need for HashMaps of Player objects

--- src/main/java/net/cubespace/geSuitWarps/commands/WarpCommand.java
+++ src/main/java/net/cubespace/geSuitWarps/commands/WarpCommand.java
@@ -16,30 +16,30 @@ import java.util.HashMap;
 
 public class WarpCommand implements CommandExecutor {
 
-    static HashMap<Player, Location> lastLocation = new HashMap<>();
-
-
     @Override
     public boolean onCommand(final CommandSender sender, Command command,
                              String label, final String[] args) {
         final Player player = Bukkit.getPlayer(sender.getName());
         if (args.length == 1) {
             if (!player.hasPermission("gesuit.warps.bypass.delay")) {
-                lastLocation.put(player, player.getLocation());
+                final Location lastLocation = player.getLocation();
 
                 player.sendMessage(ChatColor.translateAlternateColorCodes('&', "&6Teleportation will commence in &c3 seconds&6. Don't move."));
 
                 geSuitWarps.getInstance().getServer().getScheduler().runTaskLater(geSuitWarps.getInstance(), new Runnable() {
                     @Override
                     public void run() {
-
-                    if (lastLocation.get(player).getBlock().equals(player.getLocation().getBlock())) {
-                        player.sendMessage(ChatColor.GOLD + "Teleportation commencing...");
-                        WarpsManager.warpPlayer(sender, sender.getName(), args[0]);
-                    } else {
-                        player.sendMessage(ChatColor.RED + "Teleportation aborted because you moved.");
-                    }
-                    lastLocation.remove(player);
+                    	if (player.isOnline()) {
+                    		if ((lastLocation == null) || (lastLocation.getBlock() == null))
+                    			return;
+                    		
+    	                    if (lastLocation.getBlock().equals(player.getLocation().getBlock())) {
+		                        player.sendMessage(ChatColor.GOLD + "Teleportation commencing...");
+		                        WarpsManager.warpPlayer(sender, sender.getName(), args[0]);
+		                    } else {
+		                        player.sendMessage(ChatColor.RED + "Teleportation aborted because you moved.");
+		                    }
+                    	}
                     }
                 }, 60L);
                 return true;
