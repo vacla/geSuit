vacla/geSuit

commit b44d083bbfa79facf8a8db5cb62e11ef22d61167

Rework player creation logic to ensure players are only created after joining + Add feature to enable debugs with /gsdebug

src/main/java/net/cubespace/geSuit/listeners/PlayerListener.java
@@ -6,7 +6,6 @@ import net.cubespace.geSuit.managers.PlayerManager;
 import net.cubespace.geSuit.objects.GSPlayer;
 import net.md_5.bungee.api.ProxyServer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
-import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.event.ServerConnectedEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -18,26 +17,14 @@ import java.util.concurrent.TimeUnit;
 public class PlayerListener implements Listener {
 
     @EventHandler(priority = EventPriority.LOW)
-    public void playerLogin(PostLoginEvent e) throws SQLException {
-        if (!PlayerManager.onlinePlayers.containsKey(e.getPlayer().getName())) {
-            PlayerManager.loadPlayer(e.getPlayer());
-        }
-    }
-
-    @EventHandler(priority = EventPriority.LOW)
     public void playerLogin(ServerConnectedEvent e) throws SQLException {
-        GSPlayer p = PlayerManager.getPlayer(e.getPlayer().getName());
-        if (p.firstConnect()) {
-            if (ConfigManager.main.BroadcastProxyConnectionMessages) {
-                PlayerManager.sendBroadcast(ConfigManager.messages.PLAYER_CONNECT_PROXY.replace("{player}", p.getName()));
-            }
+        GSPlayer p = PlayerManager.loadPlayer(e.getPlayer());
 
-            if (ConfigManager.main.MOTD_Enabled) {
-                PlayerManager.sendMessageToTarget(e.getPlayer().getName(), ConfigManager.messages.MOTD.replace("{player}", p.getName()));
-            }
-
-            p.connected();
+        if (ConfigManager.main.MOTD_Enabled) {
+            PlayerManager.sendMessageToTarget(e.getPlayer().getName(), ConfigManager.messages.MOTD.replace("{player}", p.getName()));
         }
+
+        p.connected();
     }
 
     @EventHandler(priority = EventPriority.LOW)

src/main/java/net/cubespace/geSuit/Utilities.java
@@ -1,12 +1,17 @@
 package net.cubespace.geSuit;
 
 import com.google.common.net.InetAddresses;
+
+import java.io.ByteArrayInputStream;
+import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+
 import net.cubespace.geSuit.profile.Profile;
 import net.cubespace.geSuit.tasks.DatabaseUpdateRowUUID;
 import net.md_5.bungee.api.ChatColor;
@@ -49,4 +54,24 @@ public class Utilities {
     {
         ProxyServer.getInstance().getScheduler().runAsync(geSuit.instance, new DatabaseUpdateRowUUID(id, playerName));
     }
+
+    public static String dumpPacket(String channel, String direction, byte[] bytes, boolean consoleOutput) {
+		String data = "";
+		//ByteArrayInputStream ds = new ByteArrayInputStream(bytes);
+		//DataInputStream di = new DataInputStream(ds);
+		// Read upto 20 parameters from the stream and load them into the string list
+		for (int x = 0; x < bytes.length; x++) {
+			byte c = bytes[x];
+			if (c >= 32 && c <= 126) {
+				data += (char) c; 
+			} else {
+				data += "\\x" + Integer.toHexString(c);
+			}
+		}
+		
+		if (consoleOutput) {
+			geSuit.instance.getLogger().info("geSuit DEBUG: [" + channel + "] " + direction + ": " + data);
+		}
+		return data;
+	}
 }

src/main/java/net/cubespace/geSuit/commands/DebugCommand.java
@@ -1,0 +1,34 @@
+package net.cubespace.geSuit.commands;
+
+import net.cubespace.geSuit.geSuit;
+import net.cubespace.geSuit.managers.ConfigManager;
+import net.cubespace.geSuit.managers.PlayerManager;
+import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.plugin.Command;
+
+/**
+ * Command: /gsreload Permission needed: gesuit.reload or gesuit.admin Arguments: none What does it do: Reloads every config
+ */
+public class DebugCommand extends Command
+{
+
+    public DebugCommand()
+    {
+        super("gsdebug");
+    }
+
+    @Override
+    public void execute(CommandSender sender, String[] args)
+    {
+        if (!(sender.hasPermission("gesuit.debug") || sender.hasPermission("gesuit.admin"))) {
+            PlayerManager.sendMessageToTarget(sender, ConfigManager.messages.NO_PERMISSION);
+
+            return;
+        }
+
+        // Toggle debug
+        geSuit.instance.setDebugEnabled(!geSuit.instance.isDebugEnabled());
+        
+        PlayerManager.sendMessageToTarget(sender, "geSuit debug is now: " + (geSuit.instance.isDebugEnabled() ? "ENABLED" : "DISABLED"));
+    }
+}

src/main/java/net/cubespace/geSuit/configs/Messages.java
@@ -18,7 +18,8 @@ public class Messages extends Config {
     public String PLAYER_CONNECT_PROXY = "{player}&e has joined the server!";
     public String PLAYER_DISCONNECT_PROXY = "{player}&e has left the server!";
     public String PLAYER_DOES_NOT_EXIST = "&c" + "That player does not exist";
-    public String PLAYER_LOAD = "Loaded player &9{player}";
+    public String PLAYER_LOAD = "Loaded player &9{player}&7 ({uuid})";
+    public String PLAYER_CREATE = "Created player &b{player}&7 ({uuid})";
     public String PLAYER_UNLOAD = "Unloaded player &c{player}";
     public String PLAYER_NOT_ONLINE = "&c" + "That player is not online";
     public String NO_PERMISSION = "&c" + "You do not have permission to use that command";

src/main/java/net/cubespace/geSuit/geSuit.java
@@ -1,6 +1,7 @@
 package net.cubespace.geSuit;
 
 import net.cubespace.geSuit.commands.BanCommand;
+import net.cubespace.geSuit.commands.DebugCommand;
 import net.cubespace.geSuit.commands.MOTDCommand;
 import net.cubespace.geSuit.commands.ReloadCommand;
 import net.cubespace.geSuit.commands.SeenCommand;
@@ -28,6 +29,7 @@ public class geSuit extends Plugin
 
     public static geSuit instance;
     public static ProxyServer proxy;
+    private boolean DebugEnabled = false;
 
     public void onEnable()
     {
@@ -60,6 +62,7 @@ public class geSuit extends Plugin
         proxy.getPluginManager().registerCommand(this, new UnbanCommand());
         proxy.getPluginManager().registerCommand(this, new BanCommand());
         proxy.getPluginManager().registerCommand(this, new ReloadCommand());
+        proxy.getPluginManager().registerCommand(this, new DebugCommand());
     }
 
     private void registerListeners()
@@ -86,4 +89,12 @@ public class geSuit extends Plugin
     {
         DatabaseManager.connectionPool.closeConnections();
     }
+
+	public boolean isDebugEnabled() {
+		return DebugEnabled;
+	}
+
+	public void setDebugEnabled(boolean debugEnabled) {
+		DebugEnabled = debugEnabled;
+	}
 }

src/main/java/net/cubespace/geSuit/listeners/BansMessageListener.java
@@ -4,6 +4,9 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.sql.SQLException;
+
+import net.cubespace.geSuit.Utilities;
+import net.cubespace.geSuit.geSuit;
 import net.cubespace.geSuit.managers.BansManager;
 import net.cubespace.geSuit.managers.LoggingManager;
 import net.md_5.bungee.api.connection.Server;
@@ -26,6 +29,11 @@ public class BansMessageListener implements Listener {
         if (!event.getTag().equalsIgnoreCase("geSuitBans")) {
             return;
         }
+
+		// Message debugging (can be toggled live)
+		if (geSuit.instance.isDebugEnabled()) {
+			Utilities.dumpPacket(event.getTag(), "SEND", event.getData(), true);
+		}
 
         event.setCancelled(true);
         DataInputStream in = new DataInputStream(new ByteArrayInputStream(event.getData()));

src/main/java/net/cubespace/geSuit/listeners/HomesMessageListener.java
@@ -2,6 +2,7 @@ package net.cubespace.geSuit.listeners;
 
 import net.cubespace.geSuit.FeatureDetector;
 import net.cubespace.geSuit.Utilities;
+import net.cubespace.geSuit.geSuit;
 import net.cubespace.geSuit.managers.DatabaseManager;
 import net.cubespace.geSuit.managers.HomesManager;
 import net.cubespace.geSuit.managers.LoggingManager;
@@ -32,6 +33,11 @@ public class HomesMessageListener implements Listener {
         if ( !event.getTag().equalsIgnoreCase( "geSuitHomes" ) ) {
             return;
         }
+
+		// Message debugging (can be toggled live)
+		if (geSuit.instance.isDebugEnabled()) {
+			Utilities.dumpPacket(event.getTag(), "RECV", event.getData(), true);
+		}
 
         event.setCancelled( true );
 

src/main/java/net/cubespace/geSuit/listeners/PortalsMessageListener.java
@@ -4,6 +4,9 @@ import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.sql.SQLException;
+
+import net.cubespace.geSuit.Utilities;
+import net.cubespace.geSuit.geSuit;
 import net.cubespace.geSuit.managers.ConfigManager;
 import net.cubespace.geSuit.managers.LoggingManager;
 import net.cubespace.geSuit.managers.PlayerManager;
@@ -27,6 +30,12 @@ public class PortalsMessageListener implements Listener {
         if (!event.getTag().equalsIgnoreCase("geSuitPortals")) {
             return;
         }
+
+        // Message debugging (can be toggled live)
+		if (geSuit.instance.isDebugEnabled()) {
+			Utilities.dumpPacket(event.getTag(), "RECV", event.getData(), true);
+		}
+
         event.setCancelled(true);
 
         DataInputStream in = new DataInputStream(new ByteArrayInputStream(event.getData()));

commit 85068afa5c0674a79025830f2d0306089cd0e766

Broadcast for PLAYER_CONNECT_PROXY message was accidentally broken long ago in b44d083bbfa79facf8a8db5cb62e11ef22d61167 (Fixes issue #15)

src/main/java/net/cubespace/geSuit/listeners/PlayerListener.java
@@ -52,6 +52,9 @@ public class PlayerListener implements Listener {
     		final GSPlayer p = PlayerManager.confirmJoin(e.getPlayer());
     		p.setServer(e.getServer().getInfo().getName());
     		
+    		if (ConfigManager.main.BroadcastProxyConnectionMessages) {
+    			PlayerManager.sendBroadcast(ConfigManager.messages.PLAYER_CONNECT_PROXY.replace("{player}", p.getName()));
+    		}
     		final boolean newspawn = p.isNewSpawn();
 
 			// Check if an existing player has a "newspawn" flag... send them to new player spawn