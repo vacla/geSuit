e623e069b9fda1c34dcca77d49ac0457158cb254
Schmoller <steven.schmoll@gmail.com>
2015-04-22 14:32:45 +1000

    Added Ban and Unban events

---
+++ common/src/main/java/net/cubespace/geSuit/core/events/moderation/GlobalBanEvent.java
@@ -0,0 +1,46 @@
+package net.cubespace.geSuit.core.events.moderation;
+
+import java.net.InetAddress;
+
+import net.cubespace.geSuit.core.GlobalPlayer;
+import net.cubespace.geSuit.core.events.GSEvent;
+import net.cubespace.geSuit.core.objects.BanInfo;
+
+public class GlobalBanEvent extends GSEvent {
+    private BanInfo<?> ban;
+    public GlobalBanEvent(BanInfo<?> ban) {
+        this.ban = ban;
+    }
+    
+    public BanInfo<?> getBan() {
+        return ban;
+    }
+    
+    public GlobalPlayer getPlayer() {
+        if (isPlayerBan()) {
+            return (GlobalPlayer)ban.getWho();
+        } else {
+            return null;
+        }
+    }
+    
+    public InetAddress getAddress() {
+        if (isIPBan()) {
+            return (InetAddress)ban.getWho();
+        } else {
+            return null;
+        }
+    }
+    
+    public boolean isPlayerBan() {
+        return ban.getWho() instanceof GlobalPlayer;
+    }
+    
+    public boolean isIPBan() {
+        return ban.getWho() instanceof InetAddress;
+    }
+    
+    public static Object getHandlerList() {
+        return getHandlerList(GlobalBanEvent.class);
+    }
+}
