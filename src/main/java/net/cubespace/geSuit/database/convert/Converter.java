3550e7715797f7d488d399f976476570a10abd5b
addstar <addstar@addstar.com.au>
2015-05-11 18:19:46 +1000

    Fix reported issues with BungeeSuite converter and new "firstonline" field

--- src/main/java/net/cubespace/geSuit/database/convert/Converter.java
+++ src/main/java/net/cubespace/geSuit/database/convert/Converter.java
@@ -18,7 +18,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
