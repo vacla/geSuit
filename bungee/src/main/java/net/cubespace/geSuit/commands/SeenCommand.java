a4cf5c6ee046115a64175b7e4eaa79c286ff4d9b
Schmoller <steven.schmoll@gmail.com>
2015-06-18 14:00:31 +1000

    Implemented /seen command

--- bungee/src/main/java/net/cubespace/geSuit/commands/SeenCommand.java
+++ bungee/src/main/java/net/cubespace/geSuit/commands/SeenCommand.java
@@ -1,9 +1,22 @@
 package net.cubespace.geSuit.commands;
 
+import java.net.InetAddress;
+import java.text.DateFormat;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.google.common.collect.Maps;
+
+import au.com.addstar.bc.BungeeChat;
+import net.cubespace.geSuit.Utilities;
+import net.cubespace.geSuit.geSuit;
 import net.cubespace.geSuit.core.Global;
+import net.cubespace.geSuit.core.GlobalPlayer;
+import net.cubespace.geSuit.core.objects.BanInfo;
 import net.cubespace.geSuit.managers.ConfigManager;
-import net.cubespace.geSuit.managers.PlayerManager;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.ProxyServer;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.plugin.Command;
 
 /**
@@ -17,20 +30,125 @@ public class SeenCommand extends Command {
         super("seen");
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public void execute(CommandSender sender, String[] args) {
         if (!(sender.hasPermission("gesuit.seen") || sender.hasPermission("gesuit.admin"))) {
-            PlayerManager.sendMessageToTarget(sender, Global.getMessages().get("player.no-permission"));
-
+            sender.sendMessage(Global.getMessages().get("player.no-permission"));
             return;
         }
+        
+        if (args.length != 1) {
+            sender.sendMessage("/seen <player>");
+            return;
+        }
+        
+        GlobalPlayer player = Global.getOfflinePlayer(args[0]);
+        if (player == null) {
+            sender.sendMessage(Global.getMessages().get("player.unknown", "player", args[0]));
+            return;
+        }
+        
+        boolean seeExtra = sender.hasPermission("gesuit.seen.extra");
+        boolean seeVanish = sender.hasPermission("gesuit.seen.vanish");
+        
+        ProxiedPlayer pPlayer = ProxyServer.getInstance().getPlayer(player.getUniqueId());
+        boolean online = pPlayer != null;
 
-        throw new UnsupportedOperationException("Not implemented yet");
-//        if (args.length == 0) {
-//            PlayerManager.sendMessageToTarget(sender, ConfigManager.messages.BUNGEE_COMMAND_SEEN_USAGE);
-//            return;
-//        }
-//
-//        PlayerManager.sendMessageToTarget(sender, PlayerManager.getLastSeeninfos(args[0], sender.hasPermission("gesuit.seen.extra"), sender.hasPermission("gesuit.seen.vanish")));
+        // Vanished and not online
+        if (ConfigManager.main.BungeeChatIntegration && !seeVanish) {
+            if (BungeeChat.instance.getSyncManager().getPropertyBoolean(pPlayer, "VNP:vanished", false) && !BungeeChat.instance.getSyncManager().getPropertyBoolean(pPlayer, "VNP:online", true)) {
+                online = false;
+            }
+        }
+        
+        Map<String, Object> items = Maps.newLinkedHashMap();
+
+        // Do a ban check
+        if (player.isBanned()) {
+            BanInfo<GlobalPlayer> nameBan = player.getBanInfo();
+            if (nameBan.isTemporary()) {
+                if (nameBan.getUntil() > System.currentTimeMillis()) {
+                    items.put("Temp Banned", Utilities.buildShortTimeDiffString(nameBan.getUntil() - System.currentTimeMillis(), 3) + " remaining");
+                    items.put(" Reason", nameBan.getReason());
+                    if (seeExtra) {
+                        items.put(" By", nameBan.getBannedBy());
+                    }
+                }
+            } else {
+                items.put("Banned", nameBan.getReason());
+                if (seeExtra) {
+                    items.put(" By", nameBan.getBannedBy());
+                }
+            }
+        }
+        
+        // IP ban check
+        BanInfo<InetAddress> ipBan = geSuit.getPlugin().getBanManager().getBan(player.getAddress());
+        if (ipBan != null) {
+            if (ipBan.isTemporary()) {
+                if (ipBan.getUntil() > System.currentTimeMillis()) {
+                    items.put("Temp IP Banned", Utilities.buildShortTimeDiffString(ipBan.getUntil() - System.currentTimeMillis(), 3) + " remaining");
+                    items.put(" Reason", ipBan.getReason());
+                    if (seeExtra) {
+                        items.put(" By", ipBan.getBannedBy());
+                    }
+                }
+            } else {
+                items.put("IP Banned", ipBan.getReason());
+                if (seeExtra) {
+                    items.put(" By", ipBan.getBannedBy());
+                }
+            }
+        }
+
+        if (seeExtra) {
+            if (online && pPlayer.getServer() != null) {
+                items.put("Server", pPlayer.getServer().getInfo().getName());
+            }
+            items.put("IP", player.getAddress().getHostAddress());
+
+            // Do GeoIP lookup
+            String location = geSuit.getGeoIPLookup().lookup(player.getAddress());
+
+            if (location != null) {
+                items.put("Location", location);
+            }
+        }
+
+        // Create header
+        String messageId = (online ? "seen.header.online" : "seen.header.offline");
+        
+        String fullDate;
+        String diff;
+        
+        if (online) {
+            fullDate = String.format("%s @ %s", DateFormat.getDateInstance(DateFormat.MEDIUM).format(player.getSessionJoin()), DateFormat.getTimeInstance(DateFormat.MEDIUM).format(player.getSessionJoin()));
+            diff = Utilities.buildTimeDiffString(System.currentTimeMillis() - player.getSessionJoin(), 2);
+        } else {
+            if (player.getFirstJoined() != 0) {
+                fullDate = String.format("%s @ %s", DateFormat.getDateInstance(DateFormat.MEDIUM).format(player.getLastOnline()), DateFormat.getTimeInstance(DateFormat.MEDIUM).format(player.getLastOnline()));
+                diff = Utilities.buildTimeDiffString(System.currentTimeMillis() - player.getLastOnline(), 2);
+            } else {
+                fullDate = diff = "Never";
+            }
+        }
+        
+        String message = Global.getMessages().get(
+                messageId,
+                "player", player.getDisplayName(),
+                "timediff", diff,
+                "date", fullDate
+                );
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(message);
+        // Format each item
+        for (Entry<String, Object> item : items.entrySet()) {
+            builder.append('\n');
+            builder.append(Global.getMessages().get("seen.format", "name", item.getKey(), "value", item.getValue()));
+        }
+
+        sender.sendMessage(builder.toString());
     }
 }
