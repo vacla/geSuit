b7b07e988322009002fef2ae58452ae2021fe4a2
Matthew Monroe <matt@alchemistmatt.com>
2015-11-04 23:09:49 -0800

    Check for invalid characters in new home names

--- src/main/java/net/cubespace/geSuitHomes/commands/SetHomeCommand.java
+++ src/main/java/net/cubespace/geSuitHomes/commands/SetHomeCommand.java
@@ -1,10 +1,15 @@
 package net.cubespace.geSuitHomes.commands;
 
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 
 import net.cubespace.geSuitHomes.managers.HomesManager;
+
+import javax.annotation.RegEx;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class SetHomeCommand implements CommandExecutor {
 
@@ -14,6 +19,14 @@ public class SetHomeCommand implements CommandExecutor {
 		if(args.length==0){
 			HomesManager.setHome(sender, "home");
 		}else{
+			Pattern invalidChar = Pattern.compile("[^a-zA-Z0-9_-]");
+			Matcher reMatch = invalidChar.matcher(args[0]);
+
+			if (reMatch.find()) {
+				sender.sendMessage(ChatColor.RED + "Invalid character in new home name: " + ChatColor.YELLOW + reMatch.group(0));
+				return true;
+			}
+
 			HomesManager.setHome(sender, args[0]);
 		}
 		return true;
