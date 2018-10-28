c56bb42ab46976b54f32363fffa670bf0ecebbff
Narimm <benjicharlton@gmail.com>
2018-10-28 21:52:28 +1000

    Fix for NPE previously thrown when player was null

--- bungee/src/main/java/net/cubespace/geSuit/listeners/TeleportsListener.java
+++ bungee/src/main/java/net/cubespace/geSuit/listeners/TeleportsListener.java
@@ -1,15 +1,25 @@
 package net.cubespace.geSuit.listeners;
 
+import net.cubespace.geSuit.geSuit;
 import net.cubespace.geSuit.managers.PlayerManager;
+import net.cubespace.geSuit.objects.GSPlayer;
 import net.cubespace.geSuit.pluginmessages.LeavingServer;
 import net.md_5.bungee.api.event.ServerConnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
+import net.md_5.bungee.event.EventPriority;
 
 public class TeleportsListener implements Listener {
-    @EventHandler(priority=(byte)255)
+    @EventHandler(priority = EventPriority.HIGH)
     public void serverChange(ServerConnectEvent e) {
-        if (!e.isCancelled() && e.getPlayer().getServer() != null)
-            LeavingServer.execute(PlayerManager.getPlayer(e.getPlayer().getName(), true));
+        if (!e.isCancelled() && e.getPlayer().getServer() != null) {
+            GSPlayer player = PlayerManager.getPlayer(e.getPlayer().getUniqueId());
+            if (player == null) player = PlayerManager.getPlayer(e.getPlayer().getName());
+            if (player == null) {
+                geSuit.getInstance().getLogger().warning("Player: " + e.getPlayer() + " could not be found by the Gesuit PlayerManager. Could not execute leaving Server.");
+                return;
+            }
+            LeavingServer.execute(player);
+        }
     }
 }
